f0180f8139f1a22580bf6d6d2534deef5b406eae
Kristen Smith <kristen.smith@joinroot.com>
2021-04-20 13:35:49 -0400

    add details block around parent TOCs

--- test/build-parent-toc.js
+++ test/build-parent-toc.js
@@ -13,7 +13,7 @@ function assertPathChanges({ t, changeList, expectations }) {
 
   expectations.forEach(({ expectedData, expectedPath }) => {
     const { data } = pickFileChange({ changeList, expectedPath }) ?? {};
-    t.deepEqual(data, expectedData)
+    t.deepEqual(data, expectedData, expectedPath)
   })
 }
 
@@ -31,11 +31,14 @@ test('\nBuild Parent Toc', function (t) {
         expectedData: [
           '<!-- START doctoc generated TOC please keep comment here to allow auto update -->',
           '<!-- DON\'T EDIT THIS SECTION, INSTEAD RE-RUN doctoc TO UPDATE -->',
+          '<details><summary>Full Table of Contents</summary>',
+          '',
           'Table of Contents',
           '',
           '- [This is a test document with a number](./test_file.20200412.md#this-is-a-test-document-with-a-number)',
           '- [This is a test document](./test_file.md#this-is-a-test-document)',
           '',
+          '</details>',
           '<!-- END doctoc generated TOC please keep comment here to allow auto update -->',
         ].join('\n'),
       },
@@ -44,11 +47,14 @@ test('\nBuild Parent Toc', function (t) {
         expectedData: [
           '<!-- START doctoc generated TOC please keep comment here to allow auto update -->',
           '<!-- DON\'T EDIT THIS SECTION, INSTEAD RE-RUN doctoc TO UPDATE -->',
+          '<details><summary>Full Table of Contents</summary>',
+          '',
           'Table of Contents',
           '',
           '- [This is a test document with a number](./low-level-folder/test_file.20200412.md#this-is-a-test-document-with-a-number)',
           '- [This is a test document](./low-level-folder/test_file.md#this-is-a-test-document)',
           '',
+          '</details>',
           '<!-- END doctoc generated TOC please keep comment here to allow auto update -->',
         ].join('\n'),
       },
@@ -57,12 +63,15 @@ test('\nBuild Parent Toc', function (t) {
         expectedData: [
           '<!-- START doctoc generated TOC please keep comment here to allow auto update -->',
           '<!-- DON\'T EDIT THIS SECTION, INSTEAD RE-RUN doctoc TO UPDATE -->',
+          '<details><summary>Full Table of Contents</summary>',
+          '',
           'Table of Contents',
           '',
           '- [Top-level README](#top-level-readme)',
           '- [This is a test document with a number](./mid-level-folder/low-level-folder/test_file.20200412.md#this-is-a-test-document-with-a-number)',
           '- [This is a test document](./mid-level-folder/low-level-folder/test_file.md#this-is-a-test-document)',
           '',
+          '</details>',
           '<!-- END doctoc generated TOC please keep comment here to allow auto update -->',
           '',
           '# Top-level README',
